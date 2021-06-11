7ed0be3414412b4848806cc6b94f063272e154a5
dmelackov <dm.elackov@gmail.com>
2021-06-11 18:50:37 +0300

    Style fixes

--- app/Site/SiteMain.py
+++ app/Site/SiteMain.py
@@ -1,6 +1,5 @@
 import app.DataBase.db as MainDB
 import app.DataBase.methods as DataBaseMethods
-import json
 from flask import Flask, url_for, request, render_template, redirect, abort, send_file
 import datetime
 from flask_login import LoginManager, login_manager, login_required, login_user, logout_user, current_user
@@ -8,6 +7,8 @@ from io import BytesIO
 from app.Site.forms.user import LoginForm
 from peewee import fn
 from flask import jsonify
+from app.Site.api import api
+
 
 class FlaskSite:
     class ParamsManager:
@@ -42,6 +43,8 @@ class FlaskSite:
         self.app.jinja_env.auto_reload = True
         self.app.config['TEMPLATES_AUTO_RELOAD'] = True
 
+        self.app.register_blueprint(api, url_prefix='/api')
+
     def startFlask(self):
         self.app.run(port=80, host="0.0.0.0")
 
@@ -68,7 +71,8 @@ class FlaskSite:
         def login():
             form = LoginForm()
             if form.validate_on_submit():
-                user = MainDB.Profile.select().where(fn.lower(MainDB.Profile.Username) == form.login.data.lower())
+                user = MainDB.Profile.select().where(
+                    fn.lower(MainDB.Profile.Username) == form.login.data.lower())
                 if len(user) and user[0].check_password(form.password.data):
                     login_user(user[0], remember=form.remember_me.data)
                     return redirect("/")
@@ -76,10 +80,3 @@ class FlaskSite:
                                        message="Неправильный логин или пароль",
                                        form=form, **self.ParamsManagerObject.getParams())
             return render_template('login.html', form=form, **self.ParamsManagerObject.getParams())
-
-        @self.app.route('/api/getPlayers/')
-        @self.app.route('/api/getPlayers/<searchStr>')
-        @login_required
-        def getPlayers(searchStr=""):
-            players = DataBaseMethods.searchPlayer(searchStr)
-            return jsonify(list(map(lambda x: x.getJsonInfo(), players)))
