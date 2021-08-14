becec139334f9a39898b4837b68895b28b8d73e4
dmelackov <dm.elackov@gmail.com>
2021-08-14 18:51:11 +0300

    settings + fixes

--- app/Site/static/js/settings.js
+++ app/Site/static/js/settings.js
@@ -1,19 +1,37 @@
-(() => {
-    const searchField = document.getElementById("PlayersSearch")
-    const playersTable = document.getElementById("players_list")
+let app = null;
+document.addEventListener("DOMContentLoaded", async() => {
+    let res = await fetch("/static/html/settings_page_content.html");
+    document.getElementById("app_content").innerHTML = await res.text();
 
+    app = new Vue({
+        el: "#app_content",
+        data() {
+            return {
+                ExtendedLobbySet: false,
+                CustomAutoChoiceSet: false,
+                TeamTCountSet: 0,
+                TeamDCountSet: 0,
+                TeamHCountSet: 0,
+                Team1NameSet: "",
+                Team2NameSet: ""
+            }
+        },
+        computed: {
+            ExtendedLobby: {
+                set(v) {
 
-
-    searchField.addEventListener('input', updatePlayers)
-
-    updatePlayers()
-    async function updatePlayers() {
-        const res = await fetch('/api/getPlayers/' + searchField.value)
-        var data = await res.json()
-        playersTable.innerHTML = "";
-        var pattern = await fetch('static/html/player_pattern.html')
-        var pattern_data = await pattern.text()
-        playersTable.innerHTML = Mustache.render(pattern_data, { 'data': data })
-    }
-
-})()
+                },
+                get() {
+                    return this.ExtendedLobbySet;
+                }
+            }
+        },
+        methods: {
+            updateSettings() {
+                fetch('/api/getPlayers/')
+                    .then(response => response.json())
+                    .then(data => (this.playerList = data));
+            }
+        }
+    });
+});
