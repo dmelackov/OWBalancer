1ae981bcc9bedadc3d302c328be1647e67160333
dmelackov <dm.elackov@gmail.com>
2022-03-05 12:32:14 +0300

    remove torch, add waitress, change balance api

--- app/Site/SiteMain.py
+++ app/Site/SiteMain.py
@@ -10,28 +10,13 @@ from app.Site.api.api import api
 import logging
 from flask_wtf.csrf import CSRFProtect
 from app.params import site_port
+from waitress import serve
+
 
 module_logger = logging.getLogger("site")
 
 
 class FlaskSite:
-    class ParamsManager:
-        params = {}
-        init = False
-
-        def __init__(self):
-            pass
-
-        def getParams(self):
-            if not self.init:
-                self.initParams()
-            return self.params
-
-        def initParams(self):
-            self.init = True
-            self.params['title'] = "OWBalancer"
-            self.params['description'] = "Сайт для балансировки игроков между командами в Overwatch"
-
     def __new__(cls):
         if not hasattr(cls, 'instance'):
             cls.instance = super(FlaskSite, cls).__new__(cls)
@@ -41,7 +26,6 @@ class FlaskSite:
         module_logger.info("Site init")
         self.initFlaskConfig()
         self.initRouters()
-        self.ParamsManagerObject = self.ParamsManager()
         module_logger.info("Site init complete")
 
     def initFlaskConfig(self):
@@ -54,14 +38,16 @@ class FlaskSite:
         self.csrf = CSRFProtect()
         self.csrf.init_app(self.app)
 
-        self.app.jinja_env.auto_reload = True
-        self.app.config['TEMPLATES_AUTO_RELOAD'] = True
+        self.app.config.setdefault(
+            "WTF_CSRF_HEADERS", ["X-CSRFToken", "X-CSRF-Token", "X-Csrf-Token"])
 
     def startFlask(self):
-        self.app.run(port=site_port, host="0.0.0.0")
+        serve(self.app, host="0.0.0.0", port=site_port)
 
     def initRouters(self):
+
         @self.login_manager.user_loader
         def load_user(user_id):
             return MainDB.Profile.get(MainDB.Profile.ID == user_id)
+
         self.app.register_blueprint(api, url_prefix='/api')
