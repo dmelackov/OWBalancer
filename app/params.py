090d45d88e9ab2cae666e6ff9549875277e1528b
dmelackov <dm.elackov@gmail.com>
2022-07-05 15:00:48 +0300

    secret_key generator

--- app/params.py
+++ app/params.py
@@ -1,8 +1,17 @@
 import os
+import secrets
 
 DB_NAME = os.environ.get("DATABASE_TABLE", "owbalancer")
 db_type = os.environ.get("DATABASE_TYPE", "sqlite")
-secretKey = os.environ.get("SECRET_KEY", "VanyaPidoras")
+secretKey = os.environ.get("SECRET_KEY", None)
+if secretKey is None:
+    try:
+        with open("secret.txt") as f:
+            secretKey = f.readline()
+    except FileNotFoundError:
+        with open("secret.txt", mode="w") as f:
+            secretKey = secrets.token_urlsafe(16)
+            f.write(secretKey)
 host = os.environ.get("DATABASE_IP", "127.0.0.1")
 port = int(os.environ.get("DATABASE_PORT", "3306"))
 user = os.environ.get("DATABASE_USER_USERNAME", "root")
